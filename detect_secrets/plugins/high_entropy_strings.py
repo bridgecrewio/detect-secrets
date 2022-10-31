3e0881efdca4e0a0e6950bfa244a56bd31b69fbc
marynaKK <kabirmaryna@gmail.com>
2022-10-31 11:27:43 +0200

    added raw_context

--- detect_secrets/plugins/high_entropy_strings.py
+++ detect_secrets/plugins/high_entropy_strings.py
@@ -47,6 +47,7 @@ class HighEntropyStringsPlugin(BasePlugin, metaclass=ABCMeta):
         line: str,
         line_number: int = 0,
         context: CodeSnippet = None,
+        raw_context: CodeSnippet = None,
         enable_eager_search: bool = False,
         **kwargs: Any,
     ) -> Set[PotentialSecret]:
@@ -55,6 +56,7 @@ class HighEntropyStringsPlugin(BasePlugin, metaclass=ABCMeta):
             line=line,
             line_number=line_number,
             context=context,
+            raw_context=raw_context
         )
         if output or not enable_eager_search:
             # NOTE: We perform the limit filter at this layer (rather than analyze_string) so
