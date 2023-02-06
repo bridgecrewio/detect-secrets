4d4c37e4ae2811f265b0732f6b8a9ca687c33589
lirshindalman <lirshindelman@gmail.com>
2023-02-06 11:53:07 +0200

    a

--- detect_secrets/core/scan.py
+++ detect_secrets/core/scan.py
@@ -294,10 +294,7 @@ def _get_lines_from_file(filename: str) -> Generator[List[str], None, None]:
 
 
 def _get_lines_from_diff(diff: str) -> \
-        Generator[
-        Tuple[str, List[Tuple[int, str, bool, bool]]],
-        None, None
-]:
+        Generator[Tuple[str, List[Tuple[int, str, bool, bool]]], None, None]:
     """
     :raises: ImportError
     """
