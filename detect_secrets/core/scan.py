0f732027cbbd8d580421a3916404ed01d9217f39
lirshindalman <lirshindelman@gmail.com>
2023-02-06 11:37:41 +0200

    a

--- detect_secrets/core/scan.py
+++ detect_secrets/core/scan.py
@@ -295,8 +295,8 @@ def _get_lines_from_file(filename: str) -> Generator[List[str], None, None]:
 
 def _get_lines_from_diff(diff: str) -> \
         Generator[
-            Tuple[str, List[Tuple[int, str, Union[bool, None], Union[bool, None]]]],
-            None, None
+        Tuple[str, List[Tuple[int, str, Union[bool, None], Union[bool, None]]]],
+        None, None
         ]:
     """
     :raises: ImportError
