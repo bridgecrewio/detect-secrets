a253611fc812a97f0824ad03e1f5517d92948d94
Eliran Turgeman <elturgeman@paloaltonetworks.com>
2023-04-04 14:40:53 +0300

    lint

--- tests/plugins/azure_storage_key_test.py
+++ tests/plugins/azure_storage_key_test.py
@@ -13,7 +13,7 @@ class TestAzureStorageKeyDetector:
                 True,
             ),
             (
-                'lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==', # noqa: E501
+                'lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',  # noqa: E501
                 True,
             ),
         ],
