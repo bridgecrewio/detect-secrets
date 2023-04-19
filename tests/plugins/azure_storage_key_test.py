8f627ce82c42fbc398c5751a1d665685eb7dc818
Eliran Turgeman <50831652+Eliran-Turgeman@users.noreply.github.com>
2023-04-19 12:29:52 +0300

    Revert "fix azure storage key regex (#106)"
    This reverts commit 3ba066c237b75547225e9679aee39fa20640aea0.

--- tests/plugins/azure_storage_key_test.py
+++ tests/plugins/azure_storage_key_test.py
@@ -12,24 +12,8 @@ class TestAzureStorageKeyDetector:
                 'AccountKey=lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',  # noqa: E501
                 True,
             ),
-            (
-                'lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',  # noqa: E501
-                True,
-            ),
-            (
-                'learS3Bucket',
-                False,
-            ),
-            (
-                'anupRole',
-                False,
-            ),
-            (
-                'cret',
-                False,
-            ),
         ],
     )
     def test_analyze(self, payload, should_flag):
         logic = AzureStorageKeyDetector()
-        assert bool(logic.analyze_line(filename='mock_filename', line=payload)) == should_flag
+        assert logic.analyze_line(filename='mock_filename', line=payload)
