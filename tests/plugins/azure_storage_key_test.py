683ab1c1f3adea257de9f7210ca4172dbf070888
Eliran Turgeman <elturgeman@paloaltonetworks.com>
2023-04-19 10:39:48 +0300

    add fps

--- tests/plugins/azure_storage_key_test.py
+++ tests/plugins/azure_storage_key_test.py
@@ -16,6 +16,18 @@ class TestAzureStorageKeyDetector:
                 'lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',  # noqa: E501
                 True,
             ),
+            (
+                'learS3Bucket',
+                False
+            ),
+            (
+                'anupRole',
+                False
+            ),
+            (
+                'cret',
+                False
+            ),
         ],
     )
     def test_analyze(self, payload, should_flag):
