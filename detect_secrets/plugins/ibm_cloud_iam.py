e156fecd4811ea1769594d515660d49f918da68f
pazbechor <pbechor@paloaltonetworks.com>
2023-04-30 13:08:46 +0300

    Adding entropy calculator to IBM cloud key

--- detect_secrets/plugins/ibm_cloud_iam.py
+++ detect_secrets/plugins/ibm_cloud_iam.py
@@ -1,16 +1,17 @@
-from typing import Union
+from typing import Union, Generator
 
 import requests
 
 from ..constants import VerifiedResult
 from .base import RegexBasedDetector
+from .high_entropy_strings import Base64HighEntropyString
 
 
 class IbmCloudIamDetector(RegexBasedDetector):
     """Scans for IBM Cloud IAM Key."""
 
     secret_type = 'IBM Cloud IAM Key'
-
+    IBM_KEY_ENTROPY_GRADE = 4
     # opt means optional
     opt_ibm_cloud_iam = r'(?:ibm(?:_|-|)cloud(?:_|-|)iam|cloud(?:_|-|)iam|' + \
         r'ibm(?:_|-|)cloud|ibm(?:_|-|)iam|ibm|iam|cloud|)'
@@ -27,11 +28,19 @@ class IbmCloudIamDetector(RegexBasedDetector):
         ),
     ]
 
+    high_entropy_plugin = Base64HighEntropyString()
+
     def verify(self, secret: str) -> VerifiedResult:
         response = verify_cloud_iam_api_key(secret)
 
         return VerifiedResult.VERIFIED_TRUE if response.status_code == 200 \
             else VerifiedResult.VERIFIED_FALSE
+
+    def analyze_string(self, string: str) -> Generator[str, None, None]:
+        for match in RegexBasedDetector.analyze_string(self, string):
+            entropy_result = IbmCloudIamDetector.high_entropy_plugin.calculate_shannon_entropy(match)
+            if entropy_result > IbmCloudIamDetector.IBM_KEY_ENTROPY_GRADE:
+                yield match
 
 
 def verify_cloud_iam_api_key(apikey: Union[str, bytes]) -> requests.Response:  # pragma: no cover
@@ -51,3 +60,5 @@ def verify_cloud_iam_api_key(apikey: Union[str, bytes]) -> requests.Response:  #
         },
     )
     return response
+
+
