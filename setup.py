f8fd8d2b381e6cb3f47ad1beaa4f3b5d74cb192e
Omry Mendelovich <omry155@gmail.com>
2024-12-03 18:08:36 +0200

    change name in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', encoding='utf-8') as f:
 
 
 setup(
-    name='bc_detect_secrets',
+    name='bc-detect-secrets',
     python_requires='>=3.8',
     packages=find_packages(exclude=(['test*', 'tmp*'])),
     version=VERSION,
