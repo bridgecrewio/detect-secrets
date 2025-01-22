e2641908e8f36fcea297c74d09a34ff7dbfb4869
razreik <razreik@paloaltonetworks.com>
2025-01-22 10:37:14 +0200

    remove slack webhook

--- detect_secrets/plugins/slack.py
+++ detect_secrets/plugins/slack.py
@@ -18,31 +18,17 @@ class SlackDetector(RegexBasedDetector):
 
     denylist = (
         # Slack Token
-        re.compile(r'xox(?:a|b|p|o|s|r)-(?:\d+-)+[a-z0-9]+', flags=re.IGNORECASE),
-        # Slack Webhooks
-        re.compile(
-            r'https://hooks\.slack\.com/services/T[a-zA-Z0-9_]+/B[a-zA-Z0-9_]+/[a-zA-Z0-9_]+',
-            flags=re.IGNORECASE | re.VERBOSE,
-        ),
+        re.compile(r'xox(?:a|b|p|o|s|r)-(?:\d+-)+[a-z0-9]+', flags=re.IGNORECASE)
     )
 
     def verify(self, secret: str) -> VerifiedResult:  # pragma: no cover
-        if secret.startswith('https://hooks.slack.com/services/T'):
-            response = requests.post(
-                secret,
-                json={
-                    'text': '',
-                },
-            )
-            valid = response.text in ['missing_text_or_fallback_or_attachments', 'no_text']
-        else:
-            response = requests.post(
-                'https://slack.com/api/auth.test',
-                data={
-                    'token': secret,
-                },
-            ).json()
-            valid = cast(Dict[str, Any], response)['ok']
+        response = requests.post(
+            'https://slack.com/api/auth.test',
+            data={
+                'token': secret,
+            },
+        ).json()
+        valid = cast(Dict[str, Any], response)['ok']
 
         return (
             VerifiedResult.VERIFIED_TRUE
