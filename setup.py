5478b229b2ed5c904e68af6a96c5093a47feba91
Taylor <tsmith.v11@gmail.com>
2025-01-24 09:46:28 -0800

    Add 3.13 tests

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
         'Topic :: Software Development',
